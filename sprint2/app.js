cb5b2a7b2b5b752e13d35c8434c42ca227a55f2c
JuanSilva27 <juan.silva.0270@gmail.com>
2021-08-26 19:51:16 -0300

    se aagregaron las demas rutas

--- sprint2/app.js
+++ sprint2/app.js
@@ -5,6 +5,10 @@ const path= require("path")
 
 app.use(express.static('public'))
 
+app.get("/carrito", (req,res)=> {res.sendFile(path.join(__dirname,"views","carrito.html"))})
 app.get("/", (req,res)=> {res.sendFile(path.join(__dirname,"views","home.html"))})
+app.get("/producto", (req,res)=> {res.sendFile(path.join(__dirname,"views","producto.html"))})
+app.get("/register", (req,res)=> {res.sendFile(path.join(__dirname,"views","register.html"))})
+app.get("/login", (req,res)=> {res.sendFile(path.join(__dirname,"views","login.html"))})
 
 app.listen(port, ()=> console.log("se levanto server en el puerto " +port))
