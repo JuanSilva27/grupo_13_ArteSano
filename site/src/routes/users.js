369b45e59ce3002638702e1166c8ae53a2cebef1
Liza Benitez <leonetkd@gmail.com>
2021-09-18 21:56:01 -0300

    organizacion de carpetas, en /routes/users nuestro app

---
+++ site/src/routes/users.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
+
+/* const express= require("express")
+const app= express()
+const port= 3030
+const path= require("path")
+
+app.use(express.static('public'))
+app.set("view engine", "ejs");
+
+
+app.get("/carrito", (req,res)=> {res.sendFile(path.join(__dirname,"views","carrito.html"))})
+app.get("/", (req,res)=> {res.sendFile(path.join(__dirname,"views","home.html"))})
+app.get("/producto", (req,res)=> {res.sendFile(path.join(__dirname,"views","producto.html"))})
+app.get("/register", (req,res)=> {res.sendFile(path.join(__dirname,"views","register.html"))})
+app.get("/login", (req,res)=> {res.sendFile(path.join(__dirname,"views","login.html"))})
+
+app.listen(port, ()=> console.log("se levanto server en el puerto " +port)) */
