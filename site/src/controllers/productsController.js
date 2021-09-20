a59e24f1bd3ae478c3d57ccf4b686c3840245325
JuanSilva27 <juan.silva.0270@gmail.com>
2021-09-19 23:54:06 -0300

    se modulariso parte de relacionados

--- site/src/controllers/productsController.js
+++ site/src/controllers/productsController.js
@@ -1,9 +1,11 @@
+const relacionados=require("../views/data/relacionados.json")
+
 module.exports={
     carrito:function(req, res, next) {
-        res.render('carrito');
+        res.render('carrito',{relacionados});
       },
     products:function(req, res, next) {
-        res.render('products');
+        res.render('products',{relacionados});
       }
 }
 
