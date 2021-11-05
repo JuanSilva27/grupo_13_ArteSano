6a796f2b39db4f2ca37628cc63b1e2c10f1679ed
JuanSilva27 <juan.silva.0270@gmail.com>
2021-11-04 21:53:45 -0300

    se creo middleware del admin

--- site/src/middlewares/adminValidate.js
+++ site/src/middlewares/adminValidate.js
@@ -2,6 +2,6 @@ module.exports = (req, res, next) => {
     if (req.session.userLog.rol === "admin"  ){
         next()
     } else {
-        res.render('users/guestUser')
+        res.redirect("/")
     }
 }
