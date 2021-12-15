9f1a850839e9b465045dba7cded1d10d187be247
JuanSilva27 <juan.silva.0270@gmail.com>
2021-12-15 18:00:39 -0300

    se agregaron mas validaciones front-end en el registro de usuario

--- site/src/middlewares/validate.js
+++ site/src/middlewares/validate.js
@@ -1,9 +1,12 @@
 const {check} = require("express-validator")
+const db = require('../database/models')
 module.exports=[
     check("Nombre")
-        .notEmpty().withMessage("Debes completar este campo").bail(),
+        .notEmpty().withMessage("Debes completar este campo").bail()
+        .isLength({min:2}).withMessage("El Nombre debe tener al Menos 2 caracteres "),
     check("Apellido")
-        .notEmpty().withMessage("Debes completar este campo").bail(),
+        .notEmpty().withMessage("Debes completar este campo").bail()
+        .isLength({min:2}).withMessage("El Apellido debe tener al Menos 2 caracteres "),
     check("telefono")
         .notEmpty().withMessage("Debes completar este campo").bail(),
     check("provincia")
@@ -11,12 +14,23 @@ module.exports=[
     check("localidad")
         .notEmpty().withMessage("Debes seleccionar una localidad"),
     check("email")
-        .isEmail().withMessage("Tienes que completar el campo con un Email valido").bail(),
+        .isEmail().withMessage("Tienes que completar el campo con un Email valido").bail()
+        .custom(value=>{
+            return db.Usuarios.findOne({
+                where: {
+                    email: value
+                }
+            }).then(user=>{
+                if(user){
+                    return Promise.reject("El email ya esta registrado")
+                }
+            })
+        }),
     check("password")
         .notEmpty().withMessage("Debes completar este campo").bail()
-        .isLength({min: 6}).withMessage("La contraseña debe contener 6 caracteres") ,
+        .isLength({min: 8}).withMessage("La contraseña debe contener 8 caracteres") ,
     check("password2")
         .notEmpty().withMessage("Debes completar este campo").bail()  
-        .isLength({min: 6}).withMessage("La contraseña debe contener 6 caracteres") ,
+        .isLength({min: 8}).withMessage("La contraseña debe contener 8 caracteres") ,
 
 ]
