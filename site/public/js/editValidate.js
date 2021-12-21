d214f249a9facca9cf4075a1463212682f6ca92b
SolBena <sol.benavides95@gmail.com>
2021-12-20 21:28:51 -0300

    Se corrigen errores en validaciones del front

--- site/public/js/editValidate.js
+++ site/public/js/editValidate.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', () => {
     console.log('vinculacion exitosa');
-   
+
 
     const qs = (tag) => {
         return document.querySelector(tag)
@@ -10,35 +10,48 @@ window.addEventListener('load', () => {
     const form = qs(".formEdit")
     const text = qs("textarea")
     const select = qs("select")
-    
+
     const titulo = qs("#titulo")
     const precio = qs("#precio")
     const categorias = qs("#categorias")
     const descripcion = qs("#descripcion")
-   
-    const inputs = document.querySelectorAll("input")
+    const smallText = qs("#smallDescripcion")
+
+    const inputs = [titulo, precio]
     const button = qs('.crear')
 
-  console.log(inputs)
-  console.log(small)
-    
-form.addEventListener("submit", (e) => {
-    for ( let i = 0; i < inputs.length; i++) {
-        if (inputs[i].value === "") {
-            e.preventDefault() 
-            inputs[i].classList.remove("is-valid")
-            inputs[i].classList.add("is-invalid")
-            small[i].innerText = "No puede quedar vacio"
-            console.log(small)           
-             small[i].classList.remove("is-validSmall")
-            small[i].classList.add("is-invalidSmall")
-        } else if (inputs[i].classList.contains("is-invalid")) {
-            inputs[i].classList.remove("is-invalid")
-            inputs[i].classList.add("is-valid")
-            small[i].innerText = "Campo completado correctamente"
-            small[i].classList.remove("is-invalidSmall")
-            small[i].classList.add("is-validSmall")
-        } 
+
+    form.addEventListener("submit", (e) => {
+        for (let i = 0; i < inputs.length; i++) {
+            if (inputs[i].value.length < 6) {
+                e.preventDefault()
+                inputs[i].classList.remove("is-valid")
+                inputs[i].classList.add("is-invalid")
+                small[i].innerText = "No puede quedar vacio"
+                small[i].classList.remove("is-validSmall")
+                small[i].classList.add("is-invalidSmall")                
+            } else if (inputs[i].classList.contains("is-invalid")) {
+                inputs[i].classList.remove("is-invalid")
+                inputs[i].classList.add("is-valid")
+                small[i].innerText = "Campo completado correctamente"
+                small[i].classList.remove("is-invalidSmall")
+                small[i].classList.add("is-validSmall")
+            }
+        }
+
+    if (text.value.length < 20) {
+        e.preventDefault()
+                text.classList.remove("is-valid")
+                text.classList.add("is-invalid")
+                smallText.innerText = "La descripcion debe tener mas de 20 caracteres"
+                smallText.classList.remove("is-validSmall")
+                smallText.classList.add("is-invalidSmall") 
+    }else if (text.classList.contains("is-invalid")) {
+        text.classList.remove("is-invalid")
+        text.classList.add("is-valid")
+        smallText.innerText = "Campo completado correctamente"
+        smallText.classList.remove("is-invalidSmall")
+        smallText.classList.add("is-validSmall")
     }
 })
 
