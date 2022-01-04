b47e4e77bc92f5ad1470be000ad5f4c4cabc4f81
LucasRavalle <ravallelucas@gmail.com>
2022-01-03 22:15:47 -0300

    mostrar contraseña en register

--- site/public/js/login.js
+++ site/public/js/login.js
@@ -2,9 +2,9 @@ window.addEventListener('load', () => {
 
     const iconEye = document.querySelector('.icon-eye') 
 
-    const password = document.querySelector('#password')
-
+    const password = document.querySelector('#Password')
     
+
     iconEye.addEventListener('click', () => {
         const icon = document.getElementById('icon')
             
@@ -18,6 +18,26 @@ window.addEventListener('load', () => {
             password.type = "password"
             icon.classList.remove('fa-eye')
             icon.classList.add('fa-eye-slash')
+        }
+    })
+
+
+    const iconEye2 = document.querySelector('.icon-eye2')
+    
+    const password2 = document.querySelector('#Password2')
+
+
+    iconEye2.addEventListener('click', () => {
+        const icon2 = document.getElementById('icon2')
+
+        if (password2.type === "password"){
+            password2.type = "text"
+            icon2.classList.remove('fa-eye-slash')
+            icon2.classList.add('fa-eye')
+        } else {
+            password2.type = "password"
+            icon2.classList.remove('fa-eye')
+            icon2.classList.add('fa-eye-slash')
         }
     })
 })
