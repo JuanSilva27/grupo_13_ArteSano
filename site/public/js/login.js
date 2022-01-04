6c857248a29104e438b80e3d0eaa166ad6090501
LucasRavalle <ravallelucas@gmail.com>
2022-01-04 00:23:51 -0300

    pequeños arreglos

--- site/public/js/login.js
+++ site/public/js/login.js
@@ -2,7 +2,7 @@ window.addEventListener('load', () => {
 
     const iconEye = document.querySelector('.icon-eye') 
 
-    const password = document.querySelector('#Password')
+    const password = document.querySelector('#password')
     
 
     iconEye.addEventListener('click', () => {
@@ -21,23 +21,4 @@ window.addEventListener('load', () => {
         }
     })
 
-
-    const iconEye2 = document.querySelector('.icon-eye2')
-    
-    const password2 = document.querySelector('#Password2')
-
-
-    iconEye2.addEventListener('click', () => {
-        const icon2 = document.getElementById('icon2')
-
-        if (password2.type === "password"){
-            password2.type = "text"
-            icon2.classList.remove('fa-eye-slash')
-            icon2.classList.add('fa-eye')
-        } else {
-            password2.type = "password"
-            icon2.classList.remove('fa-eye')
-            icon2.classList.add('fa-eye-slash')
-        }
-    })
 })
