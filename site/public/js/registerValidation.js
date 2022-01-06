dd216176cc9e99fd15a888e36d1f9148e590118b
JuanSilva27 <juan.silva.0270@gmail.com>
2022-01-06 18:49:08 -0300

    se pasa fincionalidad de images.js a registerValidation.js

--- site/public/js/registerValidation.js
+++ site/public/js/registerValidation.js
@@ -357,6 +357,24 @@ window.addEventListener('load', () => {
         funcValidate(validate)
     })
 
+    const $ = id => document.getElementById(id)
+
+    /* imagen previa del producto */
+
+    $("product-img").addEventListener('change', (e) => {
+
+        let reader = new FileReader();
+        reader.readAsDataURL(e.target.files[0]);
+        let nombre = $("nombre-imagen")
+        console.log(nombre);
+        nombre.style.display = "none"
+        reader.onload = () => $("img-preview").src = reader.result
+        changeImage(e.target.name,e.target.files)
+
+        
+    
+    })
+
     console.log(validate)
 });
 
