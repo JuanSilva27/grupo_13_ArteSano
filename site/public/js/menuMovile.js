a16987022280fd9ec3284f41b4cb738f7c89cf84
SolBena <sol.benavides95@gmail.com>
2022-01-06 17:07:51 -0300

    Arreglo footer

--- site/public/js/menuMovile.js
+++ site/public/js/menuMovile.js
@@ -1,4 +1,18 @@
 window.addEventListener('load', () => {
+    console.log('Estoy vinculado');
+
+    /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+})
+
+/* window.addEventListener('load', () => {
 
     console.log("Estoy vinculado");
 
@@ -36,4 +50,10 @@ window.addEventListener('load', () => {
         }
     });
 
-})
+}) */
+
+
+
+
+
+
