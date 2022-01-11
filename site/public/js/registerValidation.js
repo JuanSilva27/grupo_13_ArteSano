b8ecc396c0a403409c94c63c850ce7c864b3ae6f
JuanSilva27 <juan.silva.0270@gmail.com>
2022-01-10 21:58:24 -0300

    se arreglan problemas con api de provincias

--- site/public/js/registerValidation.js
+++ site/public/js/registerValidation.js
@@ -367,6 +367,10 @@ window.addEventListener('load', () => {
 
 
     })
+
+    const localsOld = qs("#localsOld")
+    const oldProvincia = qs("#oldProvincia")
+    const oldLocalidad = qs("#oldLocalidad")
     fetch("https://apis.datos.gob.ar/georef/api/provincias")
         .then(resultado => {
             return resultado.json()
@@ -378,8 +382,57 @@ window.addEventListener('load', () => {
             let provAz= prov.sort()
             
             for (let i = 0; i < provAz.length; i++) {
-                provincia.innerHTML += `<option value="${provAz[i]}"> ${provAz[i]} </option>`
+                if(localsOld&&oldProvincia===provAz[i]){
+                provincia.innerHTML += `<option value="${provAz[i]}" selected> ${provAz[i]} </option>`
+                }else{
+                    provincia.innerHTML += `<option value="${provAz[i]}"> ${provAz[i]} </option>`
+                }
             }
+        })
+        .then(()=>{
+            if(provincia.value==="Ciudad Autónoma de Buenos Aires" || provincia.value==="Santiago del Estero" || provincia.value==="Santa Cruz"|| provincia.value==="Entre Ríos"){
+                fetch(`https://apis.datos.gob.ar/georef/api/localidades?provincia=${provincia.value}&max=300`)
+                .then(resultado => {
+                    
+                    return resultado.json()
+                })
+                .then(data => {
+                    localidad.innerHTML = `<option class="optionProv" value="0" disabled selected> Seleccione tu localidad </option>`
+                    let localidades= data.localidades.map(element=>{
+                        return element.nombre
+                    })
+                    console.log(localidades)
+                    let localidadesAz= localidades.sort()
+                    for (let i = 0; i < localidadesAz.length; i++) {
+                        if(localsOld&&oldLocalidad===localidadesAz[i]){
+                        localidad.innerHTML += `<option class="optionProv" value=${localidadesAz[i]} selected>${localidadesAz[i]} </option>`
+                        }else
+                        localidad.innerHTML += `<option class="optionProv" value=${localidadesAz[i]}>${localidadesAz[i]} </option>`
+                    }
+                })
+            }
+
+
+            fetch(`https://apis.datos.gob.ar/georef/api/municipios?provincia=${provincia.value}&max=300`)
+                .then(resultado => {
+                    
+                    return resultado.json()
+                })
+                .then(data => {
+                    localidad.innerHTML = `<option class="optionProv" value="0" disabled selected> Seleccione tu localidad </option>`
+                    let muni= data.municipios.map(element=>{
+                        return element.nombre
+                    })
+                    let muniAz= muni.sort()
+                    for (let i = 0; i < muniAz.length; i++) {
+                        if(localsOld&&oldLocalidad===muniAz[i]){
+                        localidad.innerHTML += `<option class="optionProv" value=${muniAz[i]} selected>${muniAz[i]} </option>`
+                        }else{
+                            localidad.innerHTML += `<option class="optionProv" value=${muniAz[i]}>${muniAz[i]} </option>`
+                        }
+                    }
+                })
+
         })
 
 
--- site/src/controllers/apiGobierno.js
+++
@@ -1,7 +0,0 @@
-/* const fetch = require("node-fetch");
-modele.exports = {
-    read: (res, req) =>{
-        fetch(https://datosgobar.github.io/georef-ar-api/)
-        .then(response => response.json())
-    }
-} */
